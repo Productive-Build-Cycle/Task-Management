7991328c91e34f88a2475da6f6b476fdb20f0a1f
Ehsan Lijani <ehsanlijani@yahoo.com>
2025-12-26 13:00:30 +0330

    + Impmenet Task Model + Implement Base Entity + Implement Enums

---
+++ src/Domain/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManagement.Domain.Common;
+
+#nullable disable
+
+public class BaseEntity<T>
+{
+    public T Id { get; set; }
+
+    public Guid GuidRow { get; set; } = Guid.NewGuid();
+
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+}
