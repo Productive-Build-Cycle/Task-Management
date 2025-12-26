5d2a7266cd69dd2eb3994a4b92111add7c79af3c
Ehsan Lijani <ehsanlijani@yahoo.com>
2025-12-26 17:24:35 +0330

    + Implement In Memmory Cache + Add Cors

--- src/Presentation/TaskManagement.Api/Program.cs
+++ src/Presentation/TaskManagement.Api/Program.cs
@@ -1,18 +1,25 @@
+using Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddMemoryCache();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+#region Add Dependecies
+
+builder.Services.RegisterApplicationConfigurations();
+
+#endregion Add Dependecies
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
     app.MapOpenApi();
-}
+
+builder.Services.AddCors();
 
 app.UseHttpsRedirection();
 
