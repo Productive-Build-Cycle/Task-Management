76b89c87911721132f213d9c97a05c2cdf777975
saeedrostami <saeedrostami1991@gmail.com>
2025-12-27 09:48:32 +0330

    complete BaseEntity.cs

--- src/Domain/Common/BaseEntity.cs
+++ src/Domain/Common/BaseEntity.cs
@@ -2,11 +2,22 @@
 
 #nullable disable
 
-public class BaseEntity<T>
+public abstract class BaseEntity<T>
 {
     public T Id { get; set; }
 
     public Guid GuidRow { get; set; } = Guid.NewGuid();
 
     public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime UpdatedDate { get; set; } = DateTime.Now;
+    public bool IsDeleted { get; set; }
+    public void MarkAsUpdated()
+    {
+        UpdatedDate = DateTime.Now;
+    }
+    public void MarkAsDeleted()
+    {
+        IsDeleted = true;
+        MarkAsUpdated();
+    }
 }
